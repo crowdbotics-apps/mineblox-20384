2c6f9a23df81fad0952eb56761543787b3c2acb3
Crowdbotics <team@crowdbotics.com>
2020-09-17 18:46:50 +0000

    Update models

--- backend/mineblox_20384/wsgi.py
+++ backend/mineblox_20384/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mineblox_20384.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mineblox_20384.settings")
 
 application = get_wsgi_application()
